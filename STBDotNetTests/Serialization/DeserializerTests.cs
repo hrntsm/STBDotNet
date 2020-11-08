d0aae5435617cfc55a03f0f4951144d25d517fc7
hiron <contact@hrntsm.com>
2020-11-08 15:29:39 +0900

    Fix DeserializeTest1

--- STBDotNetTests/Serialization/DeserializerTests.cs
+++ STBDotNetTests/Serialization/DeserializerTests.cs
@@ -2,6 +2,7 @@
 using STBDotNet.Serialization;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace STBDotNet.Serialization.Tests
@@ -19,8 +20,9 @@ namespace STBDotNet.Serialization.Tests
         [Test()]
         public void DeserializerTest1()
         {
-            var item = new Deserializer("path");
-            Assert.Equals("path", item.Path);
+            var path = "path";
+            var item = new Deserializer(path);
+            Assert.IsTrue(item.Path == path);
         }
 
         [Test()]
