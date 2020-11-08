d2b9416477bc4c1cbfb8d13a2eca30e869594e44
hiron <contact@hrntsm.com>
2020-11-08 15:15:34 +0900

    Add Deserialize and its test

---
+++ STBDotNet/Serialization/Deserializer.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Xml.Linq;
+using STBDotNet.Elements;
+using Version = STBDotNet.Elements.Version;
+
+namespace STBDotNet.Serialization
+{
+    public class Deserializer
+    {
+        public string Path { get; }
+        
+        public Deserializer()
+        {
+        }
+
+        public Deserializer(string path)
+        {
+            Path = path;
+        }
+
+        public StbElements Build()
+        {
+            var stbElements = new StbElements();
+            try
+            {
+                XDocument xDocument = XDocument.Load(Path);
+                XElement root = xDocument.Root;
+                string xmlns;
+
+                if (root != null)
+                {
+                    xmlns = root.Attribute("xmlns") != null
+                        ? "{" + (string) root.Attribute("xmlns") + "}"
+                        : string.Empty;
+
+                    var tmp = (string) root.Attribute("version");
+                    stbElements.Version = tmp.Split('.')[0] switch
+                    {
+                        "1" => Version.Stb1,
+                        "2" => Version.Stb2,
+                        _ => throw new ArgumentException("The STB version is not set")
+                    };
+                }
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException(e + "Can't read Stb file, may be the path is wrong.");
+            }
+
+            return stbElements;
+        }
+    }
+}
---
+++ STBDotNetTests/Serialization/DeserializerTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using STBDotNet.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace STBDotNet.Serialization.Tests
+{
+    [TestFixture()]
+    public class DeserializerTests
+    {
+        [Test()]
+        public void DeserializerTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void DeserializerTest1()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void BuildTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
