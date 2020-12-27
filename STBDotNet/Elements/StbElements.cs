af51595102549c4ae98d682807ad85ae5a6b2939
hrntsm <contact@hrntsm.com>
2020-12-27 22:39:08 +0900

    Add ToString method

--- STBDotNet/Elements/StbElements.cs
+++ STBDotNet/Elements/StbElements.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System.Xml.Serialization;
 using STBDotNet.Elements.StbModel;
 using STBDotNet.Elements.StbCommon;
@@ -14,6 +15,22 @@ namespace STBDotNet.Elements
         [XmlElement("StbModel")] public Model Model { get; set; } = new Model();
         [XmlElement("StbFromIfc")] public FromIfc FromIfc { get; set; } = new FromIfc();
         [XmlElement("StbExtensions")] public Extensions Extensions { get; set; } = new Extensions();
+
+        public override string ToString()
+        {
+            int numColumn = Model.Members.Columns?.Count ?? 0;
+            int numPost = Model.Members.Posts?.Count ?? 0;
+            int numGirder = Model.Members.Girders?.Count ?? 0;
+            int numBeam = Model.Members.Beams?.Count ?? 0;
+            int numBrace = Model.Members.Braces?.Count ?? 0;
+
+            var sb = new StringBuilder();
+            sb.Append($"Stb version: {Version} \n");
+            sb.Append($"Node: {Model.Nodes.Count}\n");
+            sb.Append($"Column: {numColumn}, Post: {numPost}, Girder: {numGirder}, Beam: {numBeam}, Brace: {numBrace}\n");
+            sb.Append($"Section: {Model.Sections.Count}");
+            return sb.ToString();
+        }
     }
 
     public enum Version
